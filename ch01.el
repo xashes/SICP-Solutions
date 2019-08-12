57f10b688ed479cac39a60fb4cbec97a83af2e42
xashes <kellerx0319@gmail.com>
2019-08-12 23:24:49 +0800

    ex1.8

--- ch01.el
+++ ch01.el
@@ -1,4 +1,12 @@
 ;; ex1.7
+;; -> real? real? boolean?
+(defun good-enough? (guess new-guess)
+	(< (abs (- 1
+		   (/ guess new-guess)))
+	   0.001)
+	)
+
+;; -> real? real?
 (defun sqrt-nt (x)
   (defun improve-guess (guess)
     (/ (+ guess 
@@ -6,11 +14,7 @@
        2))
   (defun sqrt-iter (guess)
     (let ((new-guess (improve-guess guess)))
-      (defun good-enough? (guess)
-	(< (abs (- 1
-		   (/ guess new-guess)))
-	   0.001))
-      (if (good-enough? guess)
+      (if (good-enough? guess new-guess)
 	  guess
 	(sqrt-iter new-guess))))
   
@@ -20,4 +24,25 @@
 (ert-deftest sqrt-nt-test ()
   (should (= (round (sqrt-nt 9)) 3.0))
   (should (= (round (sqrt-nt 36)) 6))
+  )
+
+;; ex1.8
+;; -> real? real?
+(defun cube-root (x)
+  (defun improve-guess (guess)
+    (/ (+ (* 2 guess)
+	  (/ x (expt guess 2)))
+       3.0))
+  (defun cube-root-iter (guess)
+    (let ((new-guess (improve-guess guess)))
+      (if (good-enough? guess new-guess)
+	  guess
+	(cube-root-iter new-guess))
+      ))
+  (cube-root-iter 1.0)
+  )
+
+(ert-deftest cube-root-test ()
+  (should (= (round (cube-root 1)) 1))
+  (should (= (round (cube-root 27)) 3))
   )
